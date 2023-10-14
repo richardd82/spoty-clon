8f42724f7e3da55739aed37c41f868f5acb1cd50
Richardd82 <richardd82@gmail.com>
2023-10-14 08:03:23 -0600

    GUI and Effects - Persistence Audio, Play and Puse OK

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const currentTime = new Date();
+  // console.log(currentTime)
+  const currentHour = currentTime.getHours();
+
+  let greeting = ''
+
+  if (currentHour < 12) {
+    greeting = "Buenos Días";
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes";
+  } else {
+    greeting = "Buenas noches";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
